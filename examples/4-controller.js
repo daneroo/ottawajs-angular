bd2ecd33f7b3b550a493f9f9e4547657a20b3a65
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-11-11 19:22:15 -0500

    add/filter/count to controller

--- examples/4-controller.js
+++ examples/4-controller.js
@@ -1,4 +1,4 @@
-function LogCtrl($scope) {
+function LogCtrl($scope,$filter) {
   var now=+new Date();
   $scope.entries = [
     {stamp:new Date(now-161000),level:'success',msg:'Operations Start'},
@@ -6,4 +6,18 @@ function LogCtrl($scope) {
     {stamp:new Date(now-32000),level:'warning',msg:'It\'s getting warm in here'},
     {stamp:new Date(),level:'error',msg:'Something bad happened'}
   ];
+
+  $scope.addOne = function (level,msg) {
+    var entry = {
+      stamp: new Date(),
+      level: level || 'info',
+      msg: msg || 'Hello'
+    };
+    $scope.entries.push(entry);
+  };
+
+  $scope.areVisible = function () {
+    return $filter('filter')($scope.entries,$scope.search).length;
+  }
 }
+
