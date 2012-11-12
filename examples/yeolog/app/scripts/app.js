1d96014e544337de2b5c7e65ec5895fdf6cabbfa
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-11-12 13:50:37 -0500

    yeoman init

---
+++ examples/yeolog/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var yeologApp = angular.module('yeologApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
